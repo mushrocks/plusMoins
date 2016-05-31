a35a058c58cdd6ebf27020fcb6dc93d0bd71be08
MushR0CKS <charly.husson@gmail.com>
2016-05-31 11:57:40 +0200

    Adding onResume system

--- app/src/main/java/com/example/charlyhusson/plusmoins/PlusMoinsActivity.java
+++ app/src/main/java/com/example/charlyhusson/plusmoins/PlusMoinsActivity.java
@@ -1,12 +1,17 @@
 package com.example.charlyhusson.plusmoins;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.widget.Button;
 import android.widget.*;
 import android.view.*;
+
+import org.w3c.dom.Text;
 
 public class PlusMoinsActivity extends AppCompatActivity {
 
@@ -15,21 +20,22 @@ public class PlusMoinsActivity extends AppCompatActivity {
     private TextView textA;
     private TextView textB;
     private TextView textC;
-
-
-
-
+    private TextView displaySettings;
+    public static final String mySettings = "mySettings";
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plus_moins);
+        this.getSettings(displaySettings);
 
         textA = (TextView) findViewById(R.id.a);
         textB = (TextView) findViewById(R.id.b);
         textC = (TextView) findViewById(R.id.c);
         settingsButton = (Button) findViewById(R.id.settingsButton);
+        displaySettings = (TextView) findViewById(R.id.displaySettings);
 
         textA.setOnFocusChangeListener(new View.OnFocusChangeListener() {
 
@@ -58,7 +64,17 @@ public class PlusMoinsActivity extends AppCompatActivity {
 
         });
 
+        SharedPreferences sharedpreferences = getSharedPreferences(mySettings, Context.MODE_PRIVATE);
+        this.getSettings(displaySettings);
+
     }
+
+    @Override
+    public void onResume () {
+        super.onResume();
+        this.getSettings(displaySettings);
+    }
+
 
     public void updateView(TextView c) {
         c.setText(new Double(maValue).toString());
@@ -73,4 +89,15 @@ public class PlusMoinsActivity extends AppCompatActivity {
             updateView(textC);
         }
     }
+
+    public void getSettings(TextView t) {
+        t = (TextView) findViewById(R.id.displaySettings);
+
+        SharedPreferences sharedpreferences = getSharedPreferences(mySettings, Context.MODE_PRIVATE);
+
+        t.setText("Min val: "+sharedpreferences.getAll().get("minValeur").toString()+"" +
+                "\nMax val: "+sharedpreferences.getAll().get("maxValeur").toString()+"" +
+                "\n Increment val :"+sharedpreferences.getAll().get("incrementValeur").toString());
+    }
+
 }
--- app/src/main/java/com/example/charlyhusson/plusmoins/SettingsActivity.java
+++ app/src/main/java/com/example/charlyhusson/plusmoins/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.example.charlyhusson.plusmoins;
 
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
@@ -11,7 +13,9 @@ public class SettingsActivity extends AppCompatActivity {
 
     private EditText minValeur, maxValeur,incrementValeur;
     private Button saveButton;
-
+    private TextView displaySettings;
+    public static final String mySettings = "mySettings";
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +26,32 @@ public class SettingsActivity extends AppCompatActivity {
         minValeur = (EditText) findViewById(R.id.minValeur);
         maxValeur = (EditText) findViewById(R.id.maxValeur);
         incrementValeur = (EditText) findViewById(R.id.incrementValue);
+        saveButton = (Button) findViewById(R.id.saveButton);
+        displaySettings = (TextView) findViewById(R.id.displaySettings);
+
+
+
+
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+
+                String min = minValeur.getText().toString();
+                String max = maxValeur.getText().toString();
+                String increment = incrementValeur.getText().toString();
+                SharedPreferences sharedpreferences = getSharedPreferences(mySettings, Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+                editor.putString("minValeur", min);
+                editor.putString("maxValeur", max);
+                editor.putString("incrementValeur", increment);
+                editor.apply();
+
+                Toast.makeText(SettingsActivity.this,"Settings Saved ",Toast.LENGTH_LONG).show();
+            }
+
+        });
+
 
 
 
